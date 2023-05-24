ebf8e805f0668c11705aeb956302f466b4ca1b3b
Wagner <wprodriguesbr@gmail.com>
2023-05-24 17:18:17 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
+let posicao = 0
+let posicao_alvo = 0
+let stop = true
+strip.setBrightness(127)
+strip.showRainbow(1, 360)
+basic.forever(function () {
+    while (!(stop)) {
+    	
+    }
+})
